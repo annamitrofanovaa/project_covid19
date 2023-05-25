67eec6c7e7a137ca9e215fab29b0546e5e7bbc61
mitrofanova_anna <a.mitrofanova@g.nsu.ru>
2023-05-25 19:31:29 +0300

    test

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-ds = pd.read_excel("Russia.xlsx")
-print(ds.head())
+df = pd.read_excel("Russia.xlsx")
+#print(data.head())
+
+# фильтруем строки
+df = df[df['Регион'].isin(['Бурятия', 'Оренбургская обл.', 'Ростовская обл.','Москва','Санкт-Петербург','Крым','Регион'])]
+
+# сохраняем результат в новый файл
+df.to_excel('russian_data.xlsx', index=False)
