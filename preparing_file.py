befe909f40f8ace55ebb1b5972a63dc8e1bb7974
mitrofanova_anna <a.mitrofanova@g.nsu.ru>
2023-05-25 19:46:04 +0300

    test

---
+++ preparing_file.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel("Russia.xlsx")
+#print(data.head())
+
+# фильтруем строки
+df = df[df['Регион'].isin(['Бурятия', 'Оренбургская обл.', 'Ростовская обл.','Москва','Санкт-Петербург','Крым','Регион'])]
+
+# преобразование столбца "Дата" в формат даты
+df['Дата'] = pd.to_datetime(df['Дата']).dt.date
+# сортировка по возрастанию дат
+df = df.sort_values(by='Дата')
+
+# сохраняем результат в новый файл
+df.to_excel('russian_data.xlsx', index=False)
