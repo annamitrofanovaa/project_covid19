3490a5bbc4a0b926a9547683c5cbfcc2fb94582e
mitrofanova_anna <a.mitrofanova@g.nsu.ru>
2023-05-29 20:16:35 +0300

    final....

--- hypothesis.py
+++ hypothesis.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def hypothesis(df, city, what_to_do):
   start_vaccination = pd.to_datetime('2020.12.02', format='%Y.%m.%d')
@@ -14,21 +15,21 @@ def hypothesis(df, city, what_to_do):
   plt.show()
   date_start = dataframe.iloc[0]['Дата']
   date_end = dataframe.iloc[dataframe.shape[0] - 1]['Дата']
-  y1 = dataframe[dataframe['Дата'] == date_start].iloc[0][action]
+  y1 = dataframe[dataframe['Дата'] == date_start].iloc[0][what_to_do]
   #условие на случай если y1 окажется нулем - на ноль делить нельзя
   idx = 0
   while y1 == 0:
     date_start = dataframe.iloc[idx]['Дата']
-    y1 = dataframe[dataframe['Дата'] == date_start].iloc[0][action]
+    y1 = dataframe[dataframe['Дата'] == date_start].iloc[0][what_to_do]
     idx += 1
-  yn = dataframe[dataframe['Дата'] == date_end].iloc[0][action]
-  y_sv = dataframe[dataframe['Дата'] == start_vaccination].iloc[0][action]
-  print('показания "', action, '" с начала до начала вакцинации')
+  yn = dataframe[dataframe['Дата'] == date_end].iloc[0][what_to_do]
+  y_sv = dataframe[dataframe['Дата'] == start_vaccination].iloc[0][what_to_do]
+  print('показания "', what_to_do, '" с начала до начала вакцинации')
   t = (y_sv/y1)**(1/(dataframe.shape[0] - 1))*100
   print('средний темп прироста:')
   k1 = t - 100
   print(k1, '%')
-  print('показания "', action, '" с начала вакцинации до конца')
+  print('показания "', what_to_do, '" с начала вакцинации до конца')
   t = (yn/y_sv)**(1/(dataframe.shape[0] - 1))*100
   print('средний темп прироста:')
   k2 = t - 100
